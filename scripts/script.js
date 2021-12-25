4c64c0159fae967621760287a65eec0863459a83
Israel Berko <berkois@gmail.com>
2021-12-26 01:30:32 +0200

    fix: fix all correction from code reviews

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
-let formElement = document.querySelector(".popup"); // Use the querySelector() method
+let popup = document.querySelector(".popup");
+let formElement = popup.querySelector(".popup__form");
 let editButton = document.querySelector(".profile__edit-button");
-let closeButton = document.querySelector(".popup__close-button");
+let closeButton = popup.querySelector(".popup__close-button");
 let currentName = document.querySelector(".profile__name");
 let currentJob = document.querySelector(".profile__title");
 let nameInput = formElement.querySelector(".popup__input_type_name");
@@ -10,21 +11,16 @@ function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   currentName.textContent = nameInput.value;
   currentJob.textContent = jobInput.value;
+  popup.classList.remove("popup_opened");
+  formElement.reset();
 }
 
-function showPopup(evt) {
-  evt.preventDefault();
-  formElement.classList.add("popup_opened");
+function togglePopup(evt) {
+  popup.classList.toggle("popup_opened");
   nameInput.value = currentName.textContent;
   jobInput.value = currentJob.textContent;
 }
 
-function hidePopup(evt) {
-  evt.preventDefault();
-  formElement.classList.remove("popup_opened");
-}
-
 formElement.addEventListener("submit", handleProfileFormSubmit);
-formElement.addEventListener("submit", hidePopup);
-editButton.addEventListener("click", showPopup);
-closeButton.addEventListener("click", hidePopup);
+editButton.addEventListener("click", togglePopup);
+closeButton.addEventListener("click", togglePopup);
