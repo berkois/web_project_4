6fe277685aa0a4f237d459e26dde82e9566dc379
Israel Berko <berkois@gmail.com>
2022-01-20 17:27:14 +0200

    refactor: calling evtlisteners when card added

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
+/* -------------------------------------------------------------------------- */
+/*                                Edit Profile                                */
+/* -------------------------------------------------------------------------- */
+
 // setting popup for editing the profile
 const popupEditProfile = document.querySelector(".popup_type_edit-profile");
 const formEditProfile = popupEditProfile.querySelector(".popup__form");
@@ -34,51 +38,47 @@ editButton.addEventListener("click", () => togglePopup(popupEditProfile));
 editButton.addEventListener("click", fillDefaultFormFields);
 closeButtonEditProfile.addEventListener("click", () => togglePopup(popupEditProfile));
 
+/* -------------------------------------------------------------------------- */
+/*                   Place Cards: Adding, Removing, Thumb-up                  */
+/* -------------------------------------------------------------------------- */
+
+// setting popup for adding place card
+const popupAddCard = document.querySelector(".popup_type_add-card");
+const formAddCard = popupAddCard.querySelector(".popup__form");
+const addButton = document.querySelector(".profile__add-button");
+const closeButtonAddCard = popupAddCard.querySelector(".popup__close-button");
+const placeInput = formAddCard.querySelector(".popup__input_type_place");
+const imgSrcInput = formAddCard.querySelector(".popup__input_type_img-src");
+
 // setting the cards container and template for card elements
 const cardsContainer = document.querySelector(".photos-grid__list");
 const cardTemplate = document.querySelector("#card-template").content;
-const cards = [];
-const cardDeleteButtons = [];
-const cardLikeButtons = [];
-
-// removing card
-function removeCard() {
-  cardDeleteButtons.forEach((deleteButton) => {
-    deleteButton.addEventListener("click", function (evt) {
-      const deleteTarget = evt.target;
-      const cardToDelete = deleteTarget.closest(".photos-grid__card");
-      cardToDelete.remove();
-    });
-  });
-}
-
-// like function
-function like() {
-  cardLikeButtons.forEach((likeButton) => {
-    likeButton.addEventListener("click", function (evt) {
-      const likeTarget = evt.target;
-      likeTarget.classList.toggle("heart_active");
-    });
-  });
-}
 
 // adding new card
 function getCard(cardInfo) {
+  // cloning new card from template and adding the info from input
   const cardElement = cardTemplate.querySelector(".photos-grid__card").cloneNode(true);
   cardElement.querySelector(".photos-grid__photo").src = cardInfo.link;
   cardElement.querySelector(".photos-grid__photo").alt = cardInfo.name;
   cardElement.querySelector(".photos-grid__location").textContent = cardInfo.name;
 
+  // add event listener to delete button of the newly added place card
   const cardDeleteButton = cardElement.querySelector(".photos-grid__trash");
-  cardDeleteButtons.push(cardDeleteButton);
-  removeCard();
+  cardDeleteButton.addEventListener("click", function (evt) {
+    const deleteTarget = evt.target;
+    const cardToDelete = deleteTarget.closest(".photos-grid__card");
+    cardToDelete.remove();
+  });
 
+  // add event listener to like button of the newly added place card
   const cardLikeButton = cardElement.querySelector(".heart");
-  cardLikeButtons.push(cardLikeButton);
-  like();
+  cardLikeButton.addEventListener("click", function (evt) {
+    const likeTarget = evt.target;
+    likeTarget.classList.toggle("heart_active");
+  });
 
+  // adding the new place card to the page
   cardsContainer.prepend(cardElement);
-  cards.push(cardElement);
 }
 
 // initial cards info
@@ -115,28 +115,28 @@ const initialCard = initialCards.map((card) => {
   return initialCardItem;
 });
 
-// setting popup for adding place card
-const popupAddCard = document.querySelector(".popup_type_add-card");
-const formAddCard = popupAddCard.querySelector(".popup__form");
-const addButton = document.querySelector(".profile__add-button");
-const closeButtonAddCard = popupAddCard.querySelector(".popup__close-button");
-const placeInput = formAddCard.querySelector(".popup__input_type_place");
-const imgSrcInput = formAddCard.querySelector(".popup__input_type_img-src");
-
-// event listener for add-card button
-addButton.addEventListener("click", () => togglePopup(popupAddCard));
-
-// converting input to card attributes
+// handling all the actions for submitted add-card form
 function handleAddCardSubmit(evt) {
   evt.preventDefault();
+
   const newCardInfo = {
     name: placeInput.value,
     link: imgSrcInput.value,
   };
   getCard(newCardInfo);
+
   popupAddCard.classList.toggle("popup_opened");
+  // reset the card input
   formAddCard.reset();
 }
 
+// handling all the actions for submitted add-card form
+function handleAddCardCancel() {
+  togglePopup(popupAddCard);
+  formAddCard.reset();
+}
+
+// event listener for add-card button
+addButton.addEventListener("click", () => togglePopup(popupAddCard));
 formAddCard.addEventListener("submit", handleAddCardSubmit);
-closeButtonAddCard.addEventListener("click", () => togglePopup(popupAddCard));
+closeButtonAddCard.addEventListener("click", () => handleAddCardCancel());
