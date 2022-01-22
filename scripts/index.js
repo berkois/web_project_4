0da41d28d1541eecc19069e8c75deb97153300d4
Israel Berko <berkois@gmail.com>
2022-01-22 20:08:45 +0200

    fix: some errors from code review

--- scripts/index.js
+++ scripts/index.js
@@ -12,18 +12,23 @@ const currentJob = document.querySelector(".profile__title");
 const nameInput = formEditProfile.querySelector(".popup__input_type_name");
 const jobInput = formEditProfile.querySelector(".popup__input_type_job");
 
+// showing the pop-ups
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+}
+
+// removing the pop-ups
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
+}
+
 // editing the proile from input
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   currentName.textContent = nameInput.value;
   currentJob.textContent = jobInput.value;
-  popupEditProfile.classList.toggle("popup_opened");
+  closePopup(popupEditProfile);
   formEditProfile.reset();
-}
-
-// showing/removing the pop-ups
-function togglePopup(popup) {
-  popup.classList.toggle("popup_opened");
 }
 
 // get default values from current info
@@ -34,9 +39,9 @@ function fillDefaultFormFields(evt) {
 
 // assigning functions to events for edit-form functionalities
 formEditProfile.addEventListener("submit", handleProfileFormSubmit);
-editButton.addEventListener("click", () => togglePopup(popupEditProfile));
+editButton.addEventListener("click", () => openPopup(popupEditProfile));
 editButton.addEventListener("click", fillDefaultFormFields);
-closeButtonEditProfile.addEventListener("click", () => togglePopup(popupEditProfile));
+closeButtonEditProfile.addEventListener("click", () => closePopup(popupEditProfile));
 
 /* -------------------------------------------------------------------------- */
 /*                   Place Cards: Adding, Removing, Thumb-up                  */
@@ -53,6 +58,18 @@ const imgSrcInput = formAddCard.querySelector(".popup__input_type_img-src");
 // setting the cards container and template for card elements
 const cardsContainer = document.querySelector(".photos-grid__list");
 const cardTemplate = document.querySelector("#card-template").content;
+const popupCard = document.querySelector(".photos-grid__popup");
+const popupCardPicture = popupCard.querySelector(".photos-grid__popup-image");
+const popupCardText = popupCard.querySelector(".photos-grid__popup-text");
+const popupCardCloseButton = popupCard.querySelector(".popup__close-button");
+
+// add event listener to the close button of the popup picture of the newly added place card
+popupCardCloseButton.addEventListener("click", function (evt) {
+  closePopup(popupCard);
+  popupCardPicture.src = " ";
+  popupCardPicture.alt = " ";
+  popupCardText.textContent = " ";
+});
 
 // adding new card
 function getCard(cardInfo) {
@@ -61,8 +78,6 @@ function getCard(cardInfo) {
   cardElement.querySelector(".photos-grid__photo").src = cardInfo.link;
   cardElement.querySelector(".photos-grid__photo").alt = cardInfo.name;
   cardElement.querySelector(".photos-grid__location").textContent = cardInfo.name;
-  cardElement.querySelector(".photos-grid__popup-image").src = cardInfo.link;
-  cardElement.querySelector(".photos-grid__popup-text").textContent = cardInfo.name;
 
   // add event listener to delete button of the newly added place card
   const cardDeleteButton = cardElement.querySelector(".photos-grid__trash");
@@ -80,23 +95,31 @@ function getCard(cardInfo) {
   });
 
   // add event listener to the picture of the newly added place card
-  const cardPicture = cardElement.querySelector(".photos-grid__photo");
-  cardPicture.addEventListener("click", function (evt) {
+  cardElement.querySelector(".photos-grid__photo").addEventListener("click", function (evt) {
     const pictureToPop = evt.target;
-    const popupPicture = pictureToPop.nextElementSibling;
-    togglePopup(popupPicture);
+    const dataToPop = pictureToPop.closest(".photos-grid__card");
+    const textToPop = dataToPop.querySelector(".photos-grid__location");
+    popupCardPicture.src = pictureToPop.src;
+    popupCardPicture.alt = pictureToPop.alt;
+    popupCardText.textContent = textToPop.textContent;
+    openPopup(popupCard);
   });
 
-  // add event listener to the close button of the popup picture of the newly added place card
-  const popupPictureCloseButton = cardElement.querySelector(".popup__close-button");
-  popupPictureCloseButton.addEventListener("click", function (evt) {
-    const popupClose = evt.target;
-    const popupToClose = popupClose.closest(".photos-grid__popup");
-    togglePopup(popupToClose);
-  });
+  return cardElement;
+}
 
+// Each function is supposed to perform just one task.
+// In this case, it needs to create a card and set up event listeners.
+// This function should also return a card element (return card) but not add it to the markup.
+// To add a card to the markup, you need to create a new function like renderCard()
+// that will call a function that creates cards (getCard()),
+// and its output will be added to the markup with the help of the prepend() function.
+// It can be used both in the methods for uploading new cards and for adding a user card.
+
+function renderCard(cardInfo) {
+  const placeCard = getCard(cardInfo);
   // add the new place card to the page
-  cardsContainer.prepend(cardElement);
+  cardsContainer.prepend(placeCard);
 }
 
 // initial cards info
@@ -128,9 +151,8 @@ const initialCards = [
 ];
 
 // declaring cards from initial cards info
-const initialCard = initialCards.map((card) => {
-  initialCardItem = getCard(card);
-  return initialCardItem;
+initialCards.forEach((card) => {
+  renderCard(card);
 });
 
 // handling all the actions for submitted add-card form
@@ -141,20 +163,20 @@ function handleAddCardSubmit(evt) {
     name: placeInput.value,
     link: imgSrcInput.value,
   };
-  getCard(newCardInfo);
+  renderCard(newCardInfo);
 
-  popupAddCard.classList.toggle("popup_opened");
+  closePopup(popupAddCard);
   // reset the card input
   formAddCard.reset();
 }
 
 // handling all the actions for cancelling add-card form
 function handleAddCardCancel() {
-  togglePopup(popupAddCard);
+  closePopup(popupAddCard);
   formAddCard.reset();
 }
 
 // event listener for add-card button
-addButton.addEventListener("click", () => togglePopup(popupAddCard));
+addButton.addEventListener("click", () => openPopup(popupAddCard));
 formAddCard.addEventListener("submit", handleAddCardSubmit);
 closeButtonAddCard.addEventListener("click", () => handleAddCardCancel());
